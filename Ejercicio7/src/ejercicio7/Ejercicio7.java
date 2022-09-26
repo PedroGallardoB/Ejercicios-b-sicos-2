36a1f85d3b3a4fd40c7c3c1f3c61d33762de93c2
pgallardo <pgallardo@106-08.info2>
2022-09-26 09:34:02 +0200

    fasdf

---
+++ Ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -0,0 +1,32 @@
+package ejercicio7;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+
+		 Scanner sc = new Scanner (System.in);
+		 
+		 //Pedimos los datos
+
+		System.out.println("Introduzca su nombre");	
+		String nombre = sc.nextLine();
+	
+		System.out.println("Introduzca su dirección");
+		String dirección = sc.nextLine();
+		
+		System.out.println("Introduzca su teléfono");
+		String teléfono = sc.nextLine(); 
+		
+				
+		//Mostramos los datos
+				
+				System.out.println("Nombre:  " + nombre);
+				System.out.println("Dirección: " + dirección);
+				System.out.println("Teléfono: "  + teléfono);
+				
+				
+	}
+
+}

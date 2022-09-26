df9b67858c53951707404ba382eda7082374e6cb
pgallardo <pgallardo@106-08.info2>
2022-09-26 09:35:08 +0200

    ad

---
+++ Ejercicio9/src/ejercicio9/Ejercicio9.java
@@ -0,0 +1,25 @@
+package ejercicio9;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+
+ Scanner sc = new Scanner (System.in);
+		 
+		 //Pedimos los datos
+
+		System.out.println("Conversor a pesetas, introduzca cantidad de euros.");	
+		double n1 = sc.nextDouble();
+		double n2 = n1*166.386 ;
+	
+		//Mostramos los datos
+		
+		System.out.println("Son " + n2 +" pesetas");
+		String edad = sc.nextLine();
+		
+				
+			
+	}
+}
